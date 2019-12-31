0df7d37459386ec30324382d2f615ad246f13a46
AceZhan <aceziyizhan@hotmail.com>
2019-12-30 22:37:50 -0500

    changes to backend to serve react

--- api/views.py
+++ api/views.py
@@ -1,18 +1,25 @@
-from flask import Blueprint, request
-from pylatex import Document, Section
 import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from pylatex import Document, Section
 
 main = Blueprint('main', __name__)
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
 
-@main.route('/extract_text', methods=['POST'])
+@main.route('/', methods=['GET', 'POST'])
 def process_image():
-    if 'file' not in request.files:
-        doc = Document('basic')
-        with doc.create(Section('A section')):
-            doc.append('Some regular text and some ')
 
-        doc.generate_tex(UPLOAD_FOLDER + 'newfile')
-        return 'Found file', 201
-    else:
-        return 'No File', 404
+    return render_template("index.html", token="Hello guys")
+    # if 'file' not in request.files:
+    #     doc = Document('basic')
+    #     with doc.create(Section('A section')):
+    #         doc.append('Some regular text and some ')
+
+    #     doc.generate_pdf(UPLOAD_FOLDER + 'newfile', clean_tex=False)
+    #     return 'Found file', 201
+    # else:
+    #     return 'No File', 404
+
+
+# @main.route('/download_file')
+# def download_file():
+#     return send_from_directory(UPLOAD_FOLDER, 'newfile.pdf', as_attachment=True)
