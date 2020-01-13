5b99b06cecfd1e1984fdfef3f2db4833894fda67
AceZhan <aceziyizhan@hotmail.com>
2020-01-12 23:32:36 -0500

    removed dependency on lastpage latex package

--- app/routes.py
+++ app/routes.py
@@ -83,7 +83,7 @@ def index():
                 
 
         # create latex document
-        doc = Document('basic')
+        doc = Document('basic', page_numbers=False)
         with doc.create(Section('Extracted text')):
             for page in extractedtext:
                 for index in range(0, len(page), 1):
